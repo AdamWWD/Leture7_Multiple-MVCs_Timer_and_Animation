644657cd46972c43d6c99807e576aee701182f26
AdamWD <falco1016@gmail.com>
2018-01-09 13:46:10 +0800

    back up before change

--- Leture7_Multiple MVCs_Timer_and_Animation/ViewController.swift
+++ Leture7_Multiple MVCs_Timer_and_Animation/ViewController.swift
@@ -1,25 +1,120 @@
 //
-//  ViewController.swift
-//  Leture7_Multiple MVCs_Timer_and_Animation
+//  ViewController_Standford_L1.swift
+//  TestAnyThing
 //
-//  Created by adamwang on 2018/1/9.
-//  Copyright © 2018年 adamwang. All rights reserved.
+//  Created by adamwang on 2017/11/22.
+//  Copyright © 2017年 adamwang. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var game:Concentration =
+        Concentration(numberOfPairsOfCards: self.numberOfPairsOfCards)
+    
+    var numberOfPairsOfCards:Int {
+        return(self.cardButtons.count + 1) / 2
+    }
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private(set) var flipCount: Int = 0 {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    
+    
+    private func updateFlipCountLabel(){
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth: 5.0,
+            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
+    @IBOutlet private weak var flipCountLabel: UILabel!
+    {
+        didSet{
+            updateFlipCountLabel()
+        }
+    }
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen car was not in cardButtons")
+        }
+        
+    }
+    
+    private func updateViewFromModel()
+    {
+        for index in cardButtons.indices
+        {
+            print(cardButtons.count)
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+            }
+            else
+            {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                
+            }
+        }
+    }
+    
+//    private var emojiChoices = ["🦇","😱", "🙀", "👿", "🎃", "👻", "🍭", "🍬","🍎"]
+    private var emojiChoices = "🦇😱🙀👿🎃👻🍭🍬🍎"
+    
+    private var emoji = [Card:String]()
+
+    private func emoji(for card: Card) ->String
+    {
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
+        }
+        
+        return emoji[card] ?? "?"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func flipCard(withEmoji emoji: String, on button: UIButton)
+    {
+        print("flipCard(withEmoji: \(emoji))")
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else
+        {
+            button.setTitle(emoji, for:UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
     }
+}
 
-
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
 }
 
